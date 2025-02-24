964b3def8c0b9e9b9fdfb3265c0ab6425d6f4727
Jermaine Miller <jmillerjr93@gmail.com>
2025-02-24 18:56:09 -0500

    Added Link from Router Dom / 404 Page

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "./Card";
-
+import { Link } from "react-router-dom";
 const HomeCards = () => {
 	return (
 		<>
@@ -15,11 +15,11 @@ const HomeCards = () => {
 								Browse our React jobs and start your career
 								today
 							</p>
-							<a
-								href='/jobs.html'
+							<Link
+								to='/jobs.html'
 								className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'>
 								Browse Jobs
-							</a>
+							</Link>
 						</Card>
 						<Card bg='bg-indigo-100'>
 							<h2 className='text-2xl font-bold'>
@@ -29,11 +29,11 @@ const HomeCards = () => {
 								List your job to find the perfect developer for
 								the role
 							</p>
-							<a
-								href='/add-job.html'
+							<Link
+								to='/add-job.html'
 								className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'>
 								Add Job
-							</a>
+							</Link>
 						</Card>
 					</div>
 				</div>
